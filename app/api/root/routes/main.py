c730c6d0e500f76029cfdd1584d1acbd858d5b3c
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-02-28 16:02:31 +0530

    change the structure of the api folders

--- app/api/root/routes/main.py
+++ app/api/root/routes/main.py
@@ -1,23 +1,8 @@
-from fastapi import APIRouter, HTTPException
-from app.utils.logging import setup_logger, get_logger
-from app.api.root.libraries import root
-from app.utils.response import StandardResponse, ReadLogResponse
-from fastapi.responses import StreamingResponse
-
-setup_logger()
-logger = get_logger()
-
+from fastapi import APIRouter
+from app.api.root.routes.root import routers as root_routers
 
 routers = APIRouter(
     tags=["Root"]
 )
 
-@routers.get("/", response_model=StandardResponse)
-async def read_root():
-    logger.info("Root endpoint accessed.")
-    return await root.read_root()
-
-@routers.get("/read-log", response_model=ReadLogResponse)
-async def read_log(limit: int = None):
-    logger.info("Read log endpoint accessed.")
-    return await root.read_log(limit)
+routers.include_router(root_routers)
---
+++ app/api/root/routes/root.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+from app.utils.logging import setup_logger, get_logger
+from app.api.root.libraries import root
+from app.utils.response import StandardResponse, ReadLogResponse
+from fastapi.responses import StreamingResponse
+
+setup_logger()
+logger = get_logger()
+
+
+routers = APIRouter()
+
+@routers.get("/", response_model=StandardResponse)
+async def read_root():
+    logger.info("Root endpoint accessed.")
+    return await root.read_root()
+
+@routers.get("/read-log", response_model=ReadLogResponse)
+async def read_log(limit: int = None):
+    logger.info("Read log endpoint accessed.")
+    return await root.read_log(limit)
